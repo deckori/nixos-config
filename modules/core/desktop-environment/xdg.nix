3ebfbed30ae488429423cad0ee7053b15df692b7
deckori <deckori9@gmail.com>
2026-01-30 14:34:59 +0300

    xdg: add pkg

--- modules/core/desktop-environment/xdg.nix
+++ modules/core/desktop-environment/xdg.nix
@@ -12,6 +12,7 @@
     extraPortals = with pkgs; [
       xdg-desktop-portal-gtk
       xdg-desktop-portal
+      xdg-desktop-portal-gnome
       xdg-desktop-portal-wlr
       xdg-desktop-portal-termfilechooser
     ];
