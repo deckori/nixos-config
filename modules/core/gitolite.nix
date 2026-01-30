18a2448643af8b56feede0636f550b5b4695c3c0
deckori <deckori9@gmail.com>
2026-01-30 14:34:51 +0300

    gitolite: change pubkey

--- modules/core/gitolite.nix
+++ modules/core/gitolite.nix
@@ -3,7 +3,7 @@
 {
   services.gitolite = {
     enable = true;
-    adminPubkey = "SHA256:wy9wjAa06+AySFmduIxgDXPsvAQP45GFsjX3EuK9zNs incogshift@laptop";
+    adminPubkey = "${inputs.secrets}/.ssh/gitolite.pub";
     enableGitAnnex = true;
   };
 }
