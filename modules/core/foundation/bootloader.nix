0ed39eade0c1456e65f4bddceaf32930c5ef153e
deckori <deckori9@gmail.com>
2026-01-30 14:34:03 +0300

    boot-loader: config up

--- modules/core/foundation/bootloader.nix
+++ modules/core/foundation/bootloader.nix
@@ -1,10 +1,16 @@
 { pkgs, ... }:
 {
-  boot.loader.systemd-boot.enable = true;
-  boot.loader.systemd-boot.configurationLimit = 10;
-  boot.loader.timeout = 0;
-  boot.loader.efi.canTouchEfiVariables = true;
   boot.initrd.systemd.enable = true;
   boot.kernelPackages = pkgs.linuxPackages_zen;
   boot.supportedFilesystems = [ "ntfs" ];
+
+  boot.loader.efi = {
+    canTouchEfiVariables = true;
+  };
+
+  boot.loader.systemd-boot = {
+    enable = true;
+    configurationLimit = 10;
+  };
+
 }
