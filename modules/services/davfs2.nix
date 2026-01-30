a76710ef04427343804dd037e27ddcfbfaeedde5
deckori <deckori9@gmail.com>
2026-01-30 14:33:59 +0300

    services: options fix

--- modules/services/davfs2.nix
+++ modules/services/davfs2.nix
@@ -7,28 +7,29 @@
 }:
 
 {
-  services.davfs2.enable = false;
-  environment.etc."davfs2/secrets" = lib.mkIf config.services.davfs2.enable {
-    source = "${inputs.secrets}/passwords/nextcloud/mount";
-    mode = "0600";
+  config = lib.mkIf config.services.dav2fs.enable {
+    environment.etc."davfs2/secrets" = {
+      source = "${inputs.secrets}/passwords/nextcloud/mount";
+      mode = "0600";
+    };
+
+    systemd.mounts = [
+      {
+        enable = true;
+        description = "Webdav mount point";
+        after = [ "network-online.target" ];
+        wants = [ "network-online.target" ];
+
+        what = (
+          lib.strings.removeSuffix "\n" (
+            builtins.readFile "${inputs.secrets}/passwords/nextcloud/user1-webdav-url"
+          )
+        );
+        where = "/mnt/nextcloud/user1";
+        options = "uid=1000,gid=1000,file_mode=0664,dir_mode=2775";
+        type = "davfs";
+        mountConfig.TimeoutSec = 15;
+      }
+    ];
   };
-
-  systemd.mounts = lib.mkIf config.services.davfs2.enable [
-    {
-      enable = true;
-      description = "Webdav mount point";
-      after = [ "network-online.target" ];
-      wants = [ "network-online.target" ];
-
-      what = (
-        lib.strings.removeSuffix "\n" (
-          builtins.readFile "${inputs.secrets}/passwords/nextcloud/user1-webdav-url"
-        )
-      );
-      where = "/mnt/nextcloud/user1";
-      options = "uid=1000,gid=1000,file_mode=0664,dir_mode=2775";
-      type = "davfs";
-      mountConfig.TimeoutSec = 15;
-    }
-  ];
 }
--- modules/services/nextcloud.nix
+++ modules/services/nextcloud.nix
@@ -7,36 +7,37 @@
 }:
 
 {
-  services.nextcloud = {
-    enable = true;
-    package = pkgs.nextcloud32;
-    # hostName = (lib.strings.removeSuffix "\n" (builtins.readFile "${inputs.secrets}/hosts/laptop"));
-    hostName = (lib.strings.removeSuffix "\n" (builtins.readFile "${inputs.secrets}/hosts/laptop"));
-    config = {
-      adminpassFile = "${inputs.secrets}/passwords/nextcloud/user";
-      # dbname = "nextcloud";
-      # dbuser = "nc_user";
-      # dbhost = (lib.strings.removeSuffix "\n" (builtins.readFile "${inputs.secrets}/hosts/laptop"));
-      # dbpassFile = "${inputs.secrets}/passwords/nextcloud/db";
-      dbtype = "sqlite";
+  config = lib.mkIf config.services.nextcloud.enable {
+    services.nextcloud = {
+      package = pkgs.nextcloud32;
+      # hostName = (lib.strings.removeSuffix "\n" (builtins.readFile "${inputs.secrets}/hosts/laptop"));
+      hostName = (lib.strings.removeSuffix "\n" (builtins.readFile "${inputs.secrets}/hosts/laptop"));
+      config = {
+        adminpassFile = "${inputs.secrets}/passwords/nextcloud/user";
+        # dbname = "nextcloud";
+        # dbuser = "nc_user";
+        # dbhost = (lib.strings.removeSuffix "\n" (builtins.readFile "${inputs.secrets}/hosts/laptop"));
+        # dbpassFile = "${inputs.secrets}/passwords/nextcloud/db";
+        dbtype = "sqlite";
+      };
+      extraApps = {
+        inherit (config.services.nextcloud.package.packages.apps)
+          news
+          contacts
+          calendar
+          memories
+          tasks
+          ;
+      };
+      extraAppsEnable = true;
     };
-    extraApps = {
-      inherit (config.services.nextcloud.package.packages.apps)
-        news
-        contacts
-        calendar
-        memories
-        tasks
-        ;
-    };
-    extraAppsEnable = true;
+
+    systemd.tmpfiles.rules = [
+      "f /var/lib/nextcloud/config/CAN_INSTALL 0644 nextcloud nextcloud"
+    ];
+
+    environment.systemPackages = with pkgs; [
+      nextcloud-client
+    ];
   };
-
-  systemd.tmpfiles.rules = [
-    "f /var/lib/nextcloud/config/CAN_INSTALL 0644 nextcloud nextcloud"
-  ];
-
-  environment.systemPackages = with pkgs; [
-    nextcloud-client
-  ];
 }
