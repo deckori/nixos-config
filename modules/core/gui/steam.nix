421fcc0987b4807546e4a3eb533ab0bced2561b9
deckori <deckori9@gmail.com>
2026-01-30 14:34:53 +0300

    steam: cleanup

--- modules/core/gui/steam.nix
+++ modules/core/gui/steam.nix
@@ -1,41 +1,7 @@
 {
   pkgs,
-  pkgs-unstable,
-  pkgs-32,
   ...
 }:
 {
-  programs = {
-    steam = {
-      enable = true;
-      package = pkgs.steam;
-      remotePlay.openFirewall = true;
-      dedicatedServer.openFirewall = true;
-
-      gamescopeSession.enable = true;
-
-      extraCompatPackages = [ pkgs.proton-ge-bin ];
-    };
-
-    gamescope = {
-      enable = true;
-      capSysNice = true;
-      args = [
-        "--rt"
-        "--expose-wayland"
-      ];
-    };
-
-    gamemode.enable = true;
-  };
-
-  environment = {
-    systemPackages = with pkgs; [
-      playonlinux
-      steam-run
-    ];
-    sessionVariables = {
-      STEAM_EXTRA_COMPAT_TOOLS_PATHS = "\${HOME}/.steam/root/compatibilitytools.d";
-    };
-  };
+  programs.steam.enable = true;
 }
