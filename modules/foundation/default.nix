786da448a602b05d2a6ef4b941d56b1a2a1185b8
deckori <deckori9@gmail.com>
2026-01-30 14:34:32 +0300

    foundation: setup

--- modules/foundation/default.nix
+++ modules/foundation/default.nix
@@ -2,10 +2,17 @@
 
 {
   imports = [
+    ./bluetooth.nix
+    ./bootloader.nix
+    ./hardware.nix
+    ./lanzaboote.nix
+    ./lix.nix
+    ./misc.nix
+    ./nix.nix
+    ./network.nix
+    ./security.nix
+    ./system.nix
     ./stylix
     ./user.nix
-    ./bluetooth.nix
-    ./misc.nix
-    ./network.nix
   ];
 }
