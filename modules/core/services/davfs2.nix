0368b62a59e97577774029aee128aaee20d47f54
deckori <deckori9@gmail.com>
2026-01-30 14:34:26 +0300

    dav2fs: mount points setup

--- modules/core/services/davfs2.nix
+++ modules/core/services/davfs2.nix
@@ -1,9 +1,45 @@
-{ inputs, ... }:
+{ lib, inputs, ... }:
 
 {
   services.davfs2.enable = true;
 
   environment.etc."davfs2/secrets" = {
     source = "${inputs.secrets}/passwords/nextcloud/mount";
+    mode = "0600";
   };
+
+  systemd.mounts = [
+    {
+      enable = true;
+      description = "Webdav mount point";
+      after = [ "network-online.target" ];
+      wants = [ "network-online.target" ];
+
+      what = (
+        lib.strings.removeSuffix "\n" (
+          builtins.readFile "${inputs.secrets}/passwords/nextcloud/root-webdav-url"
+        )
+      );
+      where = "/mnt/nextcloud/root";
+      options = "uid=1000,gid=1000,file_mode=0664,dir_mode=2775";
+      type = "davfs";
+      mountConfig.TimeoutSec = 15;
+    }
+    {
+      enable = true;
+      description = "Webdav mount point";
+      after = [ "network-online.target" ];
+      wants = [ "network-online.target" ];
+
+      what = (
+        lib.strings.removeSuffix "\n" (
+          builtins.readFile "${inputs.secrets}/passwords/nextcloud/user1-webdav-url"
+        )
+      );
+      where = "/mnt/nextcloud/user1";
+      options = "uid=1000,gid=1000,file_mode=0664,dir_mode=2775";
+      type = "davfs";
+      mountConfig.TimeoutSec = 15;
+    }
+  ];
 }
