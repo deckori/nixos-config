bf0e5ce57d68d3e3984fc72d5617670f8a2e45e8
deckori <deckori9@gmail.com>
2026-01-30 14:34:58 +0300

    add pkgs

--- modules/core/cli/openssh.nix
+++ modules/core/cli/openssh.nix
@@ -1,8 +1,10 @@
-{ username, ... }:
+{ username, pkgs, ... }:
 
 {
   services.gnome.gnome-keyring.enable = true;
   programs.seahorse.enable = true;
+
+  environment.systemPackages = with pkgs; [ libsecret ];
   # services.ssh-agent.enable = true;
 
   services.openssh = {
