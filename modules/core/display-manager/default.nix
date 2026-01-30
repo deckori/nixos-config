19fc29af0017897110d5e2acd37a2c27beb3d74c
deckori <deckori9@gmail.com>
2026-01-30 14:34:58 +0300

    disable nuisances

--- modules/core/display-manager/default.nix
+++ modules/core/display-manager/default.nix
@@ -1,8 +1,11 @@
-{ ... }:
+{ lib, ... }:
 
 {
   imports = [
     ./display-manager.nix
     ./sddm.nix
   ];
+
+  services.xserver.displayManager.lightdm.enable = lib.mkForce false;
+  programs.nano.enable = lib.mkForce false;
 }
