2802efe3ab7e7bc4fb678eba1ea260cf489baa1a
deckori <deckori9@gmail.com>
2026-01-30 14:34:33 +0300

    random fixes

--- modules/gui/gtk.nix
+++ modules/gui/gtk.nix
@@ -1,37 +1,44 @@
-{ pkgs, lib, ... }:
 {
-  gtk = {
-    enable = true;
-    font = {
-      name = lib.mkDefault "Maple Mono";
-      size = lib.mkForce 12;
+  pkgs,
+  username,
+  lib,
+  ...
+}:
+{
+  home-manager.users.${username} = {
+    gtk = {
+      enable = true;
+      font = {
+        name = lib.mkDefault "Maple Mono";
+        size = lib.mkForce 12;
+      };
+      # theme = {
+      #   name = "Colloid-Green-Dark-Gruvbox";
+      #   package = pkgs.colloid-gtk-theme.override {
+      #     colorVariants = [ "dark" ];
+      #     themeVariants = [ "green" ];
+      #     tweaks = [
+      #       "gruvbox"
+      #       "rimless"
+      #       "float"
+      #     ];
+      #   };
+      # };
+      iconTheme = {
+        name = "Papirus-Dark";
+        package = pkgs.papirus-icon-theme;
+      };
+      cursorTheme = {
+        name = "Bibata-Modern-Ice";
+        package = pkgs.bibata-cursors;
+        size = 24;
+      };
     };
-    # theme = {
-    #   name = "Colloid-Green-Dark-Gruvbox";
-    #   package = pkgs.colloid-gtk-theme.override {
-    #     colorVariants = [ "dark" ];
-    #     themeVariants = [ "green" ];
-    #     tweaks = [
-    #       "gruvbox"
-    #       "rimless"
-    #       "float"
-    #     ];
-    #   };
-    # };
-    iconTheme = {
-      name = "Papirus-Dark";
-      package = pkgs.papirus-icon-theme;
-    };
-    cursorTheme = {
+
+    home.pointerCursor = {
       name = "Bibata-Modern-Ice";
       package = pkgs.bibata-cursors;
       size = 24;
     };
-  };
-
-  home.pointerCursor = {
-    name = "Bibata-Modern-Ice";
-    package = pkgs.bibata-cursors;
-    size = 24;
   };
 }
