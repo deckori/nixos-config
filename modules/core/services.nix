142c864e9dcd74ec55ba3f78c2ddf8469d5e3584
deckori <deckori9@gmail.com>
2026-01-30 14:33:48 +0300

    netcloud: setup

--- modules/core/services.nix
+++ modules/core/services.nix
@@ -1,4 +1,10 @@
-{ pkgs, ... }:
+{
+  pkgs,
+  config,
+  inputs,
+  lib,
+  ...
+}:
 {
   services = {
     scanservjs.enable = true;
@@ -19,5 +25,37 @@
       enable = true;
       useRoutingFeatures = "both";
     };
+    nextcloud = {
+      enable = true;
+      package = pkgs.nextcloud32;
+      # hostName = (lib.strings.removeSuffix "\n" (builtins.readFile "${inputs.secrets}/hosts/laptop"));
+      hostName = (lib.strings.removeSuffix "\n" (builtins.readFile "${inputs.secrets}/hosts/laptop"));
+      config = {
+        adminpassFile = "${inputs.secrets}/passwords/nextcloud/user";
+        # dbname = "nextcloud";
+        # dbuser = "nc_user";
+        # dbhost = (lib.strings.removeSuffix "\n" (builtins.readFile "${inputs.secrets}/hosts/laptop"));
+        # dbpassFile = "${inputs.secrets}/passwords/nextcloud/db";
+        dbtype = "sqlite";
+      };
+      extraApps = {
+        inherit (config.services.nextcloud.package.packages.apps)
+          news
+          contacts
+          calendar
+          memories
+          tasks
+          ;
+      };
+      extraAppsEnable = true;
+    };
   };
+
+  systemd.tmpfiles.rules = [
+    "f /var/lib/nextcloud/config/CAN_INSTALL 0644 nextcloud nextcloud"
+  ];
+
+  environment.systemPackages = with pkgs; [
+    nextcloud-client
+  ];
 }
