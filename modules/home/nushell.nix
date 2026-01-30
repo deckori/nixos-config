87d3b8da6d88a3f2a4cd000324a84b7cbf940257
deckori <deckori9@gmail.com>
2026-01-30 14:34:27 +0300

    yazi: nushell shortcut setup

--- modules/home/nushell.nix
+++ modules/home/nushell.nix
@@ -85,6 +85,16 @@
 
         def "nt" [ ] {
 
+        def --env y [...args] {
+          let tmp = (mktemp -t "yazi-cwd.XXXXXX")
+          ^yazi ...$args --cwd-file $tmp
+          let cwd = (open $tmp)
+          if $cwd != "" and $cwd != $env.PWD {
+            cd $cwd
+          }
+          rm -fp $tmp
+        }
+
         notify-send --action 'y=Focus Window' test |
           if $in == "y" {
             hyprctl clients -j |
