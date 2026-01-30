c981b211727916e807fec8a8e90152682ff49ec7
deckori <deckori9@gmail.com>
2026-01-30 14:34:29 +0300

    sddm: disable

--- modules/core/desktop-environment/display-manager.nix
+++ modules/core/desktop-environment/display-manager.nix
@@ -2,15 +2,17 @@
 
 {
   services.logind.settings.Login.HandleLidSwitch = "ignore";
-  services.displayManager.sddm = {
-    enable = true;
-    package = pkgs.kdePackages.sddm;
-    theme = "${import ./sddm.nix { inherit pkgs; }}";
-    extraPackages = [ pkgs.sddm-astronaut ];
+  services.displayManager = {
+    sddm = {
+      enable = false;
+      package = pkgs.kdePackages.sddm;
+      theme = "${import ./sddm.nix { inherit pkgs; }}";
+      extraPackages = [ pkgs.sddm-astronaut ];
+    };
   };
 
   environment.systemPackages = with pkgs; [ sddm-astronaut ];
-
   # backup DE
   services.desktopManager.gnome.enable = true;
+
 }
