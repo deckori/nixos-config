acb27d94770aa7e7750b5e2575b7f306eeb8560d
deckori <deckori9@gmail.com>
2026-01-30 14:34:28 +0300

    waybar: setup niri

--- modules/home/waybar/settings.nix
+++ modules/home/waybar/settings.nix
@@ -51,6 +51,7 @@
         modules-left = [
           # "hyprland/submap"
           # "hyprland/workspaces"
+          "niri/workspaces"
           "tray"
         ];
         modules-center = [
@@ -65,6 +66,7 @@
           "network"
           "battery"
           # "hyprland/language"
+          "niri/language"
           "custom/notification"
         ];
         clock = {
