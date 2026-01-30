852da0740a99c2d19bfe998f39894f8ab7f252e5
deckori <deckori9@gmail.com>
2026-01-30 14:34:02 +0300

    color-files: fixes and files add

--- modules/home/color-files.nix
+++ modules/home/color-files.nix
@@ -1,36 +1,41 @@
-{ config, ... }:
-with config.lib.stylix;
-let
-  inherit (config.lib.formats.rasi) mkLiteral;
-  mkRgba =
-    opacity': color:
-    let
-      c = colors;
-      r = c."${color}-rgb-r";
-      g = c."${color}-rgb-g";
-      b = c."${color}-rgb-b";
-    in
-    mkLiteral "rgba ( ${r}, ${g}, ${b}, ${opacity'} % )";
-  mkRgb = mkRgba "100";
-  rofiOpacity = toString (builtins.ceil (opacity.popups * 100));
-in
+{ config, lib, ... }:
+with config.consuetudo.colors;
 {
   xdg.configFile."colors-rgb.css" = {
     text = ''
-      @define-color base00 ${rgb base00}
-      @define-color base01 ${rgb base01}
-      @define-color base02 ${rgb base02}
-      @define-color base03 ${rgb base03}
-      @define-color base04 ${rgb base04}
-      @define-color base05 ${rgb base05}
-      @define-color base08 ${rgb base08}
-      @define-color base09 ${rgb base09}
-      @define-color base0A ${rgb base0A}
-      @define-color base0B ${rgb base0B}
-      @define-color base0C ${rgb base0C}
-      @define-color base0D ${rgb base0D}
-      @define-color base0E ${rgb base0E}
-      @define-color base0F ${rgb base0F}
+      @define-color base00 rgb(${rgb "base00"})
+      @define-color base01 rgb(${rgb "base01"})
+      @define-color base02 rgb(${rgb "base02"})
+      @define-color base03 rgb(${rgb "base03"})
+      @define-color base04 rgb(${rgb "base04"})
+      @define-color base05 rgb(${rgb "base05"})
+      @define-color base08 rgb(${rgb "base08"})
+      @define-color base09 rgb(${rgb "base09"})
+      @define-color base0A rgb(${rgb "base0A"})
+      @define-color base0B rgb(${rgb "base0B"})
+      @define-color base0C rgb(${rgb "base0C"})
+      @define-color base0D rgb(${rgb "base0D"})
+      @define-color base0E rgb(${rgb "base0E"})
+      @define-color base0F rgb(${rgb "base0F"})
+    '';
+  };
+
+  xdg.configFile."colors-rgba.css" = {
+    text = ''
+      @define-color base00 rgba(${rgba "base00"})
+      @define-color base01 rgba(${rgba "base01"})
+      @define-color base02 rgba(${rgba "base02"})
+      @define-color base03 rgba(${rgba "base03"})
+      @define-color base04 rgba(${rgba "base04"})
+      @define-color base05 rgba(${rgba "base05"})
+      @define-color base08 rgba(${rgba "base08"})
+      @define-color base09 rgba(${rgba "base09"})
+      @define-color base0A rgba(${rgba "base0A"})
+      @define-color base0B rgba(${rgba "base0B"})
+      @define-color base0C rgba(${rgba "base0C"})
+      @define-color base0D rgba(${rgba "base0D"})
+      @define-color base0E rgba(${rgba "base0E"})
+      @define-color base0F rgba(${rgba "base0F"})
     '';
   };
 }
