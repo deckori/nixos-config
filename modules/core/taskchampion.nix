69d9051620169d90682b939b337550f7e418f603
deckori <deckori9@gmail.com>
2026-01-30 14:33:46 +0300

    taskchampion fixes

--- modules/core/taskchampion.nix
+++ modules/core/taskchampion.nix
@@ -1,20 +1,12 @@
-{ ... }:
+{ inputs, lib, ... }:
 
 {
   services.taskchampion-sync-server = {
     enable = true;
     openFirewall = true;
+    host = (lib.strings.removeSuffix "\n" (builtins.readFile "${inputs.secrets}/hosts/laptop"));
+    allowClientIds = [
+      (lib.strings.removeSuffix "\n" (builtins.readFile "${inputs.secrets}/client-info/taskwarrior"))
+    ];
   };
-
-  imports = [
-    ./taskchampion-crypt.nix
-    /*
-      services.taskchampion-sync-server = {
-        host = "<ip addr>";
-        allowClientIds = [
-          "<client uuid>"
-        ];
-      };
-    */
-  ];
 }
