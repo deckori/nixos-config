6ee52bf02ec95b1c106fb654c0dde7909c1b344a
deckori <deckori9@gmail.com>
2026-02-02 13:06:10 +0300

    appimage: setup

--- modules/core/gui/misc-programs.nix
+++ modules/core/gui/misc-programs.nix
@@ -3,7 +3,13 @@
 {
   programs.ladybird.enable = true;
 
+  programs.appimage = {
+    enable = true;
+    binfmt = true;
+  };
+
   environment.systemPackages = with pkgs; [
+    miraclecast
     linux-wifi-hotspot
     baobab
   ];
