38d6a158cbc8cffcde13e70235961fd049a92e47
deckori <deckori9@gmail.com>
2026-02-09 23:10:03 +0300

    rpi5: disko conf up

--- hosts/rpi5-2/disko.nix
+++ hosts/rpi5-2/disko.nix
@@ -18,51 +18,74 @@
               };
             };
 
-            root = {
+            luks = {
               size = "100%";
 
               content = {
-                type = "btrfs";
-                extraArgs = [ "-f" ]; # Override existing partition
-                # Subvolumes must set a mountpoint in order to be mounted,
-                # unless their parent is mounted
+                type = "luks";
+                name = "crypted";
+                # disable settings.keyFile if you want to use interactive password entry
+                #passwordFile = "/tmp/secret.key"; # Interactive
+                settings = {
+                  allowDiscards = true;
+                };
+                content = {
+                  type = "btrfs";
+                  extraArgs = [ "-f" ]; # Override existing partition
+                  # Subvolumes must set a mountpoint in order to be mounted,
+                  # unless their parent is mounted
 
-                subvolumes = {
-                  # Subvolume name is different from mountpoint
-                  "/rootfs" = {
-                    mountpoint = "/";
-                  };
-                  # Subvolume name is the same as the mountpoint
-                  "/home" = {
-                    mountOptions = [ "compress=zstd" ];
-                    mountpoint = "/home";
-                  };
-                  "/st" = {
-                    mountOptions = [ "compress=zstd" ];
-                    mountpoint = "/st";
-                  };
-                  "/var" = {
-                    mountpoint = "/var";
-                  };
-                  # Sub(sub)volume doesn't need a mountpoint as its parent is mounted
-                  "/var/lib" = { };
-                  # Parent is not mounted so the mountpoint must be set
-                  "/nix" = {
-                    mountOptions = [
-                      "compress=zstd"
-                      "noatime"
-                    ];
-                    mountpoint = "/nix";
-                  };
-                  # Subvolume for the swapfile
-                  "/swap" = {
-                    mountpoint = "/.swapvol";
-                    swap = {
-                      swapfile.size = "4G";
+                  subvolumes = {
+                    # Subvolume name is different from mountpoint
+                    "/rootfs" = {
+                      mountpoint = "/";
+                      mountOptions = [
+                        "compress=zstd"
+                        "noatime"
+                      ];
+                    };
+                    # Subvolume name is the same as the mountpoint
+                    "/home" = {
+                      mountOptions = [
+                        "compress=zstd"
+                        "noatime"
+                      ];
+                      mountpoint = "/home";
+                    };
+                    "/st" = {
+                      mountOptions = [
+                        "compress=zstd"
+                        "noatime"
+                      ];
+                      mountpoint = "/st";
+                    };
+                    "/var" = {
+                      mountpoint = "/var";
+                      mountOptions = [
+                        "noatime"
+                      ];
+                    };
+                    # Sub(sub)volume doesn't need a mountpoint as its parent is mounted
+                    "/var/lib" = { };
+                    # Parent is not mounted so the mountpoint must be set
+                    "/nix" = {
+                      mountOptions = [
+                        "compress=zstd"
+                        "noatime"
+                      ];
+                      mountpoint = "/nix";
+                    };
+                    # Subvolume for the swapfile
+                    "/swap" = {
+                      mountpoint = "/.swapvol";
+                      mountOptions = [ "nodatacow" ];
+                      swap = {
+                        swapfile.size = "4G";
+                      };
                     };
                   };
-                };
 
+                };
               };
 
             };
--- modules/core/gui/qbittorrent.nix
+++ modules/core/gui/qbittorrent.nix
@@ -1,8 +1,14 @@
-{ ... }:
+{ pkgs, ... }:
 
 {
   services.qbittorrent = {
     enable = true;
     webuiPort = 9999;
   };
+
+  environment.systemPackages = with pkgs; [
+    qbittorrent-nox
+    qbittorrent-cli
+    qbittorrent
+  ];
 }
