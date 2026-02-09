8deac48039db35e0ce5a5be4e4369abf906e00a2
deckori <deckori9@gmail.com>
2026-02-09 23:31:48 +0300

    rpi5: disko conf fix

--- hosts/rpi5-2/hardware-configuration.nix
+++ hosts/rpi5-2/hardware-configuration.nix
@@ -12,21 +12,21 @@
     raspberry-pi-5.display-vc4
   ];
 
-  fileSystems = {
-    "/boot/firmware" = {
-      device = "/dev/disk/by-label/FIRMWARE";
-      fsType = "vfat";
-      options = [
-        "noatime"
-        "noauto"
-        "x-systemd.automount"
-        "x-systemd.idle-timeout=1min"
-      ];
-    };
-    "/" = {
-      device = "/dev/disk/by-label/NIXOS_SD";
-      fsType = "ext4";
-      options = [ "noatime" ];
-    };
-  };
+  # fileSystems = {
+  #   "/boot/firmware" = {
+  #     device = "/dev/disk/by-label/FIRMWARE";
+  #     fsType = "vfat";
+  #     options = [
+  #       "noatime"
+  #       "noauto"
+  #       "x-systemd.automount"
+  #       "x-systemd.idle-timeout=1min"
+  #     ];
+  #   };
+  #   "/" = {
+  #     device = "/dev/disk/by-label/NIXOS_SD";
+  #     fsType = "ext4";
+  #     options = [ "noatime" ];
+  #   };
+  # };
 }
