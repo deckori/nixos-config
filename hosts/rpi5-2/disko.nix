18b60ea3a27bc9c15ea600e8d863069c130cf0d0
deckori <deckori9@gmail.com>
2026-02-09 22:41:34 +0300

    disco: config update

--- hosts/rpi5-2/disko.nix
+++ hosts/rpi5-2/disko.nix
@@ -17,13 +17,54 @@
                 mountOptions = [ "umask=0077" ];
               };
             };
+
             root = {
               size = "100%";
+
               content = {
-                type = "filesystem";
-                format = "btrfs";
-                mountpoint = "/";
+                type = "btrfs";
+                extraArgs = [ "-f" ]; # Override existing partition
+                # Subvolumes must set a mountpoint in order to be mounted,
+                # unless their parent is mounted
+
+                subvolumes = {
+                  # Subvolume name is different from mountpoint
+                  "/rootfs" = {
+                    mountpoint = "/";
+                  };
+                  # Subvolume name is the same as the mountpoint
+                  "/home" = {
+                    mountOptions = [ "compress=zstd" ];
+                    mountpoint = "/home";
+                  };
+                  "/st" = {
+                    mountOptions = [ "compress=zstd" ];
+                    mountpoint = "/st";
+                  };
+                  "/var" = {
+                    mountpoint = "/var";
+                  };
+                  # Sub(sub)volume doesn't need a mountpoint as its parent is mounted
+                  "/var/lib" = { };
+                  # Parent is not mounted so the mountpoint must be set
+                  "/nix" = {
+                    mountOptions = [
+                      "compress=zstd"
+                      "noatime"
+                    ];
+                    mountpoint = "/nix";
+                  };
+                  # Subvolume for the swapfile
+                  "/swap" = {
+                    mountpoint = "/.swapvol";
+                    swap = {
+                      swapfile.size = "4G";
+                    };
+                  };
+                };
+
               };
+
             };
           };
         };
