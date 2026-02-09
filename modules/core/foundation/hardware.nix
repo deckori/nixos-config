f1a93eedf0d47bd5982515c2408dab5bcd36a796
deckori <deckori9@gmail.com>
2026-02-10 01:51:25 +0300

    cleanup

--- modules/core/foundation/hardware.nix
+++ modules/core/foundation/hardware.nix
@@ -2,14 +2,7 @@
 {
   # OpenGL
   hardware.graphics = {
-    enable32Bit = true;
     enable = true;
-    extraPackages = with pkgs; [
-      intel-media-driver
-      (intel-vaapi-driver.override { enableHybridCodec = true; })
-      libva-vdpau-driver
-      libvdpau-va-gl
-    ];
   };
 
   hardware.enableRedistributableFirmware = true;
