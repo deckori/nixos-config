978450ec869e97fa2c9caa1e5391da6d4d38eead
deckori <deckori9@gmail.com>
2026-02-11 22:33:21 +0300

    rpi5: user conf

--- hosts/rpi5-2/users.nix
+++ hosts/rpi5-2/users.nix
@@ -8,6 +8,16 @@
 }:
 
 {
+  users.users.${username} = {
+    isNormalUser = true;
+    extraGroups = [ "wheel" ]; # Enable ‘sudo’ for the user.
+    initialPassword = (
+      lib.strings.removeSuffix "\n" (builtins.readFile "${inputs.secrets}/passwords/users/main-user")
+    );
+    openssh.authorizedKeys.keys = [
+      (lib.strings.removeSuffix "\n" (builtins.readFile "${inputs.secrets}/.ssh/rpi5-main-user.pub"))
+    ];
+  };
   # Use less privileged nixos user
   users.users.nixos = {
     isNormalUser = true;
