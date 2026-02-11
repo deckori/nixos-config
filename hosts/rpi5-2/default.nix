3b4f5cadbd67a7954d846959be9efa6211f35adb
deckori <deckori9@gmail.com>
2026-02-11 22:24:37 +0300

    rpi5: import conf

--- hosts/rpi5-2/default.nix
+++ hosts/rpi5-2/default.nix
@@ -11,8 +11,8 @@
     ./disko.nix
     ./cachix.nix
     # ../../modules/core/cli
-    # ../../modules/core/services
-    # ../../modules/core/option.nix
+    ../../modules/core/services
+    ../../modules/core/option.nix
     # ../../modules/core/misc.nix
     ../../modules/core/secrets
     # ../../modules/core/foundation
