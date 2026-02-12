66e8bc19fd3beeba9fca2bd4c892ab9d51eac059
deckori <deckori9@gmail.com>
2026-02-12 06:53:12 +0300

    rpi5: main to develop

--- flake.nix
+++ flake.nix
@@ -22,7 +22,7 @@
     nixpkgs-unstable.url = "github:NixOS/nixpkgs/nixos-unstable";
 
     nixos-raspberrypi = {
-      url = "github:nvmd/nixos-raspberrypi/main";
+      url = "github:nvmd/nixos-raspberrypi/develop";
     };
 
     nixos-anywhere = {
