6f0371de447edcd480c738907706ca4256c2839d
deckori <deckori9@gmail.com>
2026-02-12 07:24:58 +0300

    rpi5: unimport conf

--- hosts/rpi5-2/hardware-configuration.nix
+++ hosts/rpi5-2/hardware-configuration.nix
@@ -9,11 +9,11 @@
   imports = with nixos-raspberrypi.nixosModules; [
     # Hardware configuration
     nixos-raspberrypi.lib.inject-overlays-global
-    raspberry-pi-5.base
-    raspberry-pi-5.page-size-16k
-    raspberry-pi-5.display-vc4
-    raspberry-pi-5.display-rp1
-    raspberry-pi-5.bluetooth
+    # raspberry-pi-5.base
+    # raspberry-pi-5.page-size-16k
+    # raspberry-pi-5.display-vc4
+    # raspberry-pi-5.display-rp1
+    # raspberry-pi-5.bluetooth
   ];
 
   # fileSystems = {
