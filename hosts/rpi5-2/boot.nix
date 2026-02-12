78695455506afeba8e7beb3fba673dae8a70c395
deckori <deckori9@gmail.com>
2026-02-12 07:10:55 +0300

    rpi5: attempted fix

--- hosts/rpi5-2/boot.nix
+++ hosts/rpi5-2/boot.nix
@@ -14,13 +14,8 @@ in
   boot.kernelPackages = kernelBundle.linuxPackages_rpi5;
 
   nixpkgs.overlays = lib.mkAfter [
-    (self: super: {
-      # This is used in (modulesPath + "/hardware/all-firmware.nix") when at least
-      # enableRedistributableFirmware is enabled
-      # I know no easier way to override this package
+    (final: prev: {
       inherit (kernelBundle) raspberrypiWirelessFirmware;
-      # Some derivations want to use it as an input,
-      # e.g. raspberrypi-dtbs, omxplayer, sd-image-* modules
       inherit (kernelBundle) raspberrypifw;
     })
   ];
