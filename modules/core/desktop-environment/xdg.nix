0f517a43cceac6504c53df4d7d3260b5b4351e96
deckori <deckori9@gmail.com>
2026-02-14 10:55:57 +0300

    xdg: optionize config

--- modules/core/desktop-environment/xdg.nix
+++ modules/core/desktop-environment/xdg.nix
@@ -1,20 +1,27 @@
-{ inputs, pkgs, ... }:
 {
-  xdg.portal = {
-    enable = true;
-    xdgOpenUsePortal = true;
-    config = {
-      common.default = [ "gtk" ];
-      hyprland.default = [
-        "gtk"
+  pkgs,
+  config,
+  lib,
+  ...
+}:
+{
+  config = lib.mkIf config.consuetudo.wm.enable {
+    xdg.portal = {
+      enable = true;
+      xdgOpenUsePortal = true;
+      config = {
+        common.default = [ "gtk" ];
+        hyprland.default = [
+          "gtk"
+        ];
+      };
+      extraPortals = with pkgs; [
+        xdg-desktop-portal-gtk
+        xdg-desktop-portal
+        xdg-desktop-portal-gnome
+        xdg-desktop-portal-wlr
+        xdg-desktop-portal-termfilechooser
       ];
     };
-    extraPortals = with pkgs; [
-      xdg-desktop-portal-gtk
-      xdg-desktop-portal
-      xdg-desktop-portal-gnome
-      xdg-desktop-portal-wlr
-      xdg-desktop-portal-termfilechooser
-    ];
   };
 }
