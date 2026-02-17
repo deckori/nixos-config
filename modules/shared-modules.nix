874962acf066eb80378f28b286c99a6eee2a379a
deckori <deckori9@gmail.com>
2026-02-17 21:48:58 +0300

    added shared modules

---
+++ modules/shared-modules.nix
@@ -0,0 +1,10 @@
+{ inputs, ... }:
+{
+  imports = [
+    inputs.sops-nix.nixosModules.sops
+    inputs.home-manager.nixosModules.home-manager
+    inputs.nix-flatpak.nixosModules.nix-flatpak
+    inputs.stylix.nixosModules.stylix
+    inputs.nix-index-database.nixosModules.default
+  ];
+}
