ea2aa8a12c99d70e39dfcc0ff1621053d6d1b7a4
deckori <deckori9@gmail.com>
2026-02-17 22:41:56 +0300

    sddm: config fix

--- modules/core/display-manager/sddm-theme.nix
+++ modules/core/display-manager/sddm-theme.nix
@@ -1,19 +1,2 @@
 { pkgs, ... }:
 
-pkgs.stdenv.mkDerivation {
-  name = "sddm-theme";
-  src = pkgs.fetchFromGitHub {
-    owner = "Keyitdev";
-    repo = "sddm-astronaut-theme";
-    rev = "d73842c761f7d7859f3bdd80e4360f09180fad41";
-    hash = "sha256-+94WVxOWfVhIEiVNWwnNBRmN+d1kbZCIF10Gjorea9M=";
-  };
-  #The following does not work
-  #patchPhase = ''
-  #  find . -type f -name "metadata.desktop" -print0 | xargs -0 sed -i -e 's|ConfigFile=Themes/astronaut.conf|ConfigFile=Themes/pixel_sakura.conf|g'
-  #'';
-  installPhase = ''
-    mkdir -p $out
-    cp -R ./* $out/
-  '';
-}
