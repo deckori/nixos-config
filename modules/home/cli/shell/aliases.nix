908cd98ef9ba4415b9dcb1cb83c48be2a9292117
deckori <deckori9@gmail.com>
2026-02-21 14:08:45 +0300

    pkgs: add

--- modules/home/cli/shell/aliases.nix
+++ modules/home/cli/shell/aliases.nix
@@ -1,9 +1,14 @@
-{ ... }:
+{ pkgs, ... }:
 {
+  home.packages = with pkgs; [
+    delta
+  ];
+
   home.shellAliases = {
     c = "clear";
     cd = "z";
     man = "BAT_THEME='default' batman";
     tree = "eza --icons --tree --group-directories-first";
+    diff = "delta --diff-so-fancy --side-by-side";
   };
 }
