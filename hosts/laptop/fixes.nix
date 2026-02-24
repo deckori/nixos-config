b86bb2367dd2d666d6100ebee2b462ced566c7ad
deckori <deckori9@gmail.com>
2026-02-24 23:07:24 +0300

    laptop: blacklist bugged kernel module spd5118

--- hosts/laptop/fixes.nix
+++ hosts/laptop/fixes.nix
@@ -21,4 +21,9 @@
       };
     };
   };
+
+  boot.blacklistedKernelModules = [
+    "spd5118" # blacklist to avoid these issues: [  146.522972] spd5118 14-0050: Failed to write b = 0: -6    [  146.522974] spd5118 14-0050: PM: dpm_run_callback(): spd5118_resume [spd5118] returns -6     [  146.522978] spd5118 14-0050: PM: failed to resume async: error -6, Sourced from: https://github.com/maeren/red-flake-nix/blob/f7269498a62fc542f89a5186e7d5eda9ae6db189/nixos/hosts/stellaris/hardware.nix#L63
+  ];
+
 }
