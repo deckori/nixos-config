a45cca6fe1c89a27987e75469c8b9cb0b213a009
deckori <deckori9@gmail.com>
2026-02-27 12:48:31 +0300

    disable unknown config :P

--- modules/core/cli/openssh.nix
+++ modules/core/cli/openssh.nix
@@ -31,20 +31,20 @@
         maxtime = "168h"; # Do not ban for more than 1 week
         overalljails = true; # Calculate the bantime based on all the violations
       };
-      jails = {
-        apache-nohome-iptables.settings = {
-          # Block an IP address if it accesses a non-existent
-          # home directory more than 5 times in 10 minutes,
-          # since that indicates that it's scanning.
-          filter = "apache-nohome";
-          action = ''iptables-multiport[name=HTTP, port="http,https"]'';
-          logpath = "/var/log/httpd/error_log*";
-          backend = "auto";
-          findtime = 600;
-          bantime = 600;
-          maxretry = 5;
-        };
-      };
+      # jails = {
+      #   apache-nohome-iptables.settings = {
+      #     # Block an IP address if it accesses a non-existent
+      #     # home directory more than 5 times in 10 minutes,
+      #     # since that indicates that it's scanning.
+      #     filter = "apache-nohome";
+      #     action = ''iptables-multiport[name=HTTP, port="http,https"]'';
+      #     logpath = "/var/log/httpd/error_log*";
+      #     backend = "auto";
+      #     findtime = 600;
+      #     bantime = 600;
+      #     maxretry = 5;
+      #   };
+      # };
     };
   };
 }
