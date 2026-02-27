fc88dbcb666d2812184970d3ffa86d2751aef559
deckori <deckori9@gmail.com>
2026-02-27 10:55:35 +0300

    cleanup

--- modules/core/desktop-environment/xdg.nix
+++ modules/core/desktop-environment/xdg.nix
@@ -1,27 +1,23 @@
 {
   pkgs,
-  config,
-  lib,
   ...
 }:
 {
-  config = lib.mkIf config.consuetudo.wm.enable {
-    xdg.portal = {
-      enable = true;
-      xdgOpenUsePortal = true;
-      config = {
-        common.default = [ "gtk" ];
-        hyprland.default = [
-          "gtk"
-        ];
-      };
-      extraPortals = with pkgs; [
-        xdg-desktop-portal-gtk
-        xdg-desktop-portal
-        xdg-desktop-portal-gnome
-        xdg-desktop-portal-wlr
-        xdg-desktop-portal-termfilechooser
-      ];
+  xdg.portal = {
+    enable = true;
+    xdgOpenUsePortal = true;
+    config = {
+      common.default = [ "gtk" ];
+      # hyprland.default = [
+      #   "gtk"
+      # ];
     };
+    extraPortals = with pkgs; [
+      xdg-desktop-portal-gtk
+      xdg-desktop-portal
+      xdg-desktop-portal-gnome
+      xdg-desktop-portal-wlr
+      xdg-desktop-portal-termfilechooser
+    ];
   };
 }
