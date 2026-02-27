d5239fb0075283aef074050e86865323abe8945a
deckori <deckori9@gmail.com>
2026-02-27 13:17:25 +0300

    sops: secrets file path fix

--- modules/home/sops.nix
+++ modules/home/sops.nix
@@ -3,7 +3,7 @@
 {
   sops = {
     age.keyFile = "/home/${username}/.config/sops/age/keys.txt";
-    defaultSopsFile = ../secrets/files/secrets.yaml;
+    defaultSopsFile = ../core/secrets/files/secrets.yaml;
     defaultSopsFormat = "yaml";
   };
 }
