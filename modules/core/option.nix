f53c9896a42d3a8540cc974ee723905c92b17a28
deckori <deckori9@gmail.com>
2026-02-27 10:14:48 +0300

    flattened option definitions

--- modules/core/option.nix
+++ modules/core/option.nix
@@ -4,28 +4,27 @@
 }:
 
 {
-  options = {
-    consuetudo.wm.enable = lib.mkEnableOption "Desktop environment packages";
-    consuetudo.gaming-config.enable = lib.mkEnableOption "Gaming Setup Configurations";
+  options.consuetudo.wm.enable = lib.mkEnableOption "Desktop environment packages";
+  options.consuetudo.gaming-config.enable = lib.mkEnableOption "Gaming Setup Configurations";
 
-    consuetudo.gui.enable = lib.mkEnableOption "GUI environment configs";
-    consuetudo.hyprlock.enable = lib.mkEnableOption "Hyprlock with PAM";
-    consuetudo.nvidia-intel.enable = lib.mkEnableOption "Hybrid Nvidia and Intel GPU configuration";
-    consuetudo.power.enable = lib.mkEnableOption "Power related configuration";
-    consuetudo.btop-cuda.enable = lib.mkEnableOption "Install btop with cuda and a wrapper";
+  options.consuetudo.gui.enable = lib.mkEnableOption "GUI environment configs";
+  options.consuetudo.hyprlock.enable = lib.mkEnableOption "Hyprlock with PAM";
+  options.consuetudo.nvidia-intel.enable = lib.mkEnableOption "Hybrid Nvidia and Intel GPU configuration";
+  options.consuetudo.power.enable = lib.mkEnableOption "Power related configuration";
+  options.consuetudo.btop-cuda.enable = lib.mkEnableOption "Install btop with cuda and a wrapper";
 
-    consuetudo.programs.gitolite.enable = lib.mkEnableOption "Gitolite setup";
-    consuetudo.programs.niri.enable = lib.mkEnableOption "Nixified config file for niri";
+  options.consuetudo.programs.gitolite.enable = lib.mkEnableOption "Gitolite setup";
+  options.consuetudo.programs.niri.enable = lib.mkEnableOption "Nixified config file for niri";
 
-    consuetudo.colors = {
-      rgb = lib.mkOption {
-        type = lib.types.functionTo lib.types.str;
-        description = "Returns comma separated rgb values for a color. To be used in css files:)";
-      };
-      rgba = lib.mkOption {
-        type = lib.types.functionTo lib.types.str;
-        description = "Returns comma separated rgba values for a color. To be used in css files:)";
-      };
+  options.consuetudo.colors = {
+    rgb = lib.mkOption {
+      type = lib.types.functionTo lib.types.str;
+      description = "Returns comma separated rgb values for a color. To be used in css files:)";
+    };
+    rgba = lib.mkOption {
+      type = lib.types.functionTo lib.types.str;
+      description = "Returns comma separated rgba values for a color. To be used in css files:)";
     };
   };
+
 }
