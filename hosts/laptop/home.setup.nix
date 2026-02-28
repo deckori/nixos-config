76597efccb94df2165fa906094aaaa1b2f8d8637
deckori <deckori9@gmail.com>
2026-02-28 13:53:14 +0300

    pkgs: add

--- hosts/laptop/home.setup.nix
+++ hosts/laptop/home.setup.nix
@@ -1,8 +1,12 @@
-{ ... }:
+{ pkgs, ... }:
 
 {
   consuetudo.wm.enable = true;
   consuetudo.programs.niri.enable = true;
   programs.hyprlock.enable = true;
   services.hypridle.enable = true;
+
+  home.packages = with pkgs; [
+    ardour
+  ];
 }
