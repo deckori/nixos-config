4063186841e9d46a288a4f118ad5f4f8ef4ad6ec
deckori <deckori9@gmail.com>
2026-03-03 10:40:06 +0300

    sunshine: disable

--- modules/core/gui/gaming.nix
+++ modules/core/gui/gaming.nix
@@ -84,7 +84,7 @@
     ];
 
     services.sunshine = {
-      enable = true;
+      enable = false;
       autoStart = true;
       capSysAdmin = true; # only needed for Wayland -- omit this when using with Xorg
       openFirewall = true;
