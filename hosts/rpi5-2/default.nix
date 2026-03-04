9d3141e88ea2adf2422d3b27d0ce583cdc13b62a
deckori <deckori9@gmail.com>
2026-03-04 05:49:37 +0300

    rpi5: rm imports

--- hosts/rpi5-2/default.nix
+++ hosts/rpi5-2/default.nix
@@ -11,23 +11,23 @@
     ./disko.nix
     ./cachix.nix
     # ../../modules/core/cli
-    ../../modules/core/services
+    # ../../modules/core/services
     # ../../modules/core/misc.nix
     # ../../modules/core/secrets
     # ../../modules/core/foundation
-    ../../modules/core/foundation/package-manager/nix.nix
+    # ../../modules/core/foundation/package-manager/nix.nix
   ];
   # consuetudo = {
   #   # gpu.name = "hybrid-intel-nvidia";
   #   services.enable = true;
   # };
 
-  virtualisation.libvirtd.enable = false;
-  services.avahi.enable = true;
-  services.odoo.enable = false;
-  services.davfs2.enable = false;
-  services.gotenberg.enable = false;
-  services.suwayomi-server.enable = true;
-  services.nextcloud.enable = false;
-  services.taskchampion-sync-server.enable = true;
+  # virtualisation.libvirtd.enable = false;
+  # services.avahi.enable = true;
+  # services.odoo.enable = false;
+  # services.davfs2.enable = false;
+  # services.gotenberg.enable = false;
+  # services.suwayomi-server.enable = true;
+  # services.nextcloud.enable = false;
+  # services.taskchampion-sync-server.enable = true;
 }
