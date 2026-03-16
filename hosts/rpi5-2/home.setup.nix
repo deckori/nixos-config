22e358500f6bb2c8cbb268860cb79990bdbdc824
deckori <deckori9@gmail.com>
2026-03-16 21:46:09 +0300

    rpi5: rm wm stuff

--- hosts/rpi5-2/home.setup.nix
+++ hosts/rpi5-2/home.setup.nix
@@ -1,11 +1,6 @@
 { pkgs, ... }:
 
 {
-  consuetudo.wm.enable = false;
-  consuetudo.programs.niri.enable = false;
-  programs.hyprlock.enable = false;
-  services.hypridle.enable = false;
-
   # home.packages = with pkgs; [
   # ];
 }
