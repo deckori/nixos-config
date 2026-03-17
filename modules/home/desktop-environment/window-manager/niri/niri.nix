b3b60b89038415b8c169520bb1a1cff5451ff75e
deckori <deckori9@gmail.com>
2026-03-17 14:31:53 +0300

    rm redundant

--- modules/home/desktop-environment/window-manager/niri/niri.nix
+++ modules/home/desktop-environment/window-manager/niri/niri.nix
@@ -10,54 +10,6 @@
     # package = pkgs.niri;
   };
 
-  programs.niri.settings.cursor = {
-    hide-when-typing = true;
-    hide-after-inactive-ms = 1000;
-  };
-
-  programs.niri.settings.input = {
-    keyboard.numlock = true;
-    touchpad = {
-      tap = true;
-      natural-scroll = true;
-    };
-    focus-follows-mouse = {
-      enable = true;
-      max-scroll-amount = "0%";
-    };
-  };
-
-  programs.niri.settings.outputs."eDP-1" = {
-
-    mode = "1920x1080@144.001";
-    # mode = "1920x1080@60.001";
-
-    scale = 1;
-
-    transform = "normal";
-    # transform = "90";
-
-    position = {
-      x = 0;
-      y = 0;
-    };
-  };
-
-  programs.niri.settings.outputs."HDMI-A-5" = {
-
-    mode = "1920x1080@100";
-
-    scale = 0.9;
-
-    transform = "normal";
-    # transform = "90";
-
-    position = {
-      x = 0;
-      y = -1200;
-    };
-  };
-
   # xdg.configFile."niri/design/colors.kdl" = {
   #   text = with config.lib.stylix.colors.withHashtag; ''
   #     overview {
