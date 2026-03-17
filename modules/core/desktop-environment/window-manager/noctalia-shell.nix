1e600506d84ca8ac9bcc180235d701bc0350f0a4
deckori <deckori9@gmail.com>
2026-03-17 14:28:57 +0300

    noctalia-shell: rm redundant

--- modules/core/desktop-environment/window-manager/noctalia-shell.nix
+++ modules/core/desktop-environment/window-manager/noctalia-shell.nix
@@ -1,13 +1,8 @@
 { pkgs, ... }:
 
 {
-  environment.systemPackages = with pkgs; [
-    noctalia-shell
-  ];
-
   networking.networkmanager.enable = true;
   hardware.bluetooth.enable = true;
   services.power-profiles-daemon.enable = true;
   services.upower.enable = true;
-
 }
