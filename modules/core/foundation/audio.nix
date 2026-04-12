7a9fa91b50876fef7e421d48e3711a3b79443bc4
deckori <deckori9@gmail.com>
2026-04-12 18:34:43 +0300

    audio: fixes [SUCCESS]

--- modules/core/foundation/audio.nix
+++ modules/core/foundation/audio.nix
@@ -1,12 +1,22 @@
-{ ... }:
+{ pkgs, ... }:
 
 {
-  services.pulseaudio.enable = false;
+  # services.pulseaudio.enable = false;
+
   services.pipewire = {
     enable = true;
     alsa.enable = true;
     alsa.support32Bit = true;
     pulse.enable = true;
+    jack.enable = true;
     # lowLatency.enable = true;
   };
+
+  environment.systemPackages = with pkgs; [
+    alsa-utils
+    alsa-lib
+    alsa-ucm-conf
+    pipewire
+    wireplumber
+  ];
 }
