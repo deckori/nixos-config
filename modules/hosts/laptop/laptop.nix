436122c1b472f87b2be45d303cb08d84e250c156
deckori <deckori9@gmail.com>
2026-05-14 12:33:55 +0300

    laptop: import hardware config and bootLoader

--- modules/hosts/laptop/laptop.nix
+++ modules/hosts/laptop/laptop.nix
@@ -7,6 +7,8 @@
 {
   flake.nixosConfigurations.laptop = inputs.nixpkgs.lib.nixosSystem {
     modules = [
+      self.nixosModules.laptopHardwareConfiguration
+      self.nixosModules.bootLoader
     ];
   };
 }
